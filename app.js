34bfc6852172005bb8f9b50bdc0103315b47cab4
raji2004 <kingraj1344@gmail.com>
2022-10-09 17:02:12 +0100

    .

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ async function mainMail(name, email, subject, message) {
     service: "gmail",
     auth: {
       user: process.env.GMAIL_USER,
-      pass: process.env.PASSWORD,
+      pass: process.env.pass,
     },
   });
   const mailOption = {
