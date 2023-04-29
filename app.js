18675d4f44f0c1c7a17481afac7901fbd3044df4
raji2004 <kingraj1344@gmail.com>
2023-04-29 20:02:14 +0100

    s

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const User = require('./user')
 const app = express();
 
 app.use(express.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
