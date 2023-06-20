3dd838abb4f84e02f01ac9666b3c7fc7cb58a8d0
raji <kingraj1344@gmail.com>
2023-06-20 14:27:01 +0100

    d

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const User = require('./user')
 const app = express();
 
 app.use(express.json());
+
 app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 async function mainMail(name, email,) {
   const transporter = await nodeMail.createTransport({
